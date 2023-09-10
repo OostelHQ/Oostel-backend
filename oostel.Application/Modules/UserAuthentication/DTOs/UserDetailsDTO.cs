ca0fea8335d06de54a4c0fb1418a24ff9d695b6b
donkennie <ajeigbekehinde160@gmail.com>
2023-09-10 13:01:32 +0100

    add token mechanism

---
+++ oostel.Application/Modules/UserAuthentication/DTOs/UserDetailsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oostel.Application.Modules.UserAuthentication.DTOs
+{
+    public class UserDetailsDTO
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string Fullname { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ oostel.Application/Modules/UserAuthentication/Services/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using Oostel.Application.Modules.UserAuthentication.DTOs;
+using Oostel.Domain.UserAuthentication.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oostel.Application.Modules.UserAuthentication.Services
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateAccessToken(ApplicationUser user);
+        Task<UserDetailsDTO> CreateUserObject(ApplicationUser user);
+    }
+}
