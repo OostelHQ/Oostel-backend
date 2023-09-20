744e9c0af7c274fcd8ac44411c77b1de057528bb
donkennie <ajeigbekehinde160@gmail.com>
2023-09-20 06:13:24 +0100

    create filter search extension

---
+++ oostel.API/Extensions/HttpExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace Oostel.API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage,
+          int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new
+            {
+                currentPage,
+                itemsPerPage,
+                totalItems,
+                totalPages
+            };
+            response.Headers.Add("X-Pagination", JsonSerializer.Serialize(paginationHeader));
+
+        }
+    }
+}
---
+++ oostel.Application/RequestFilters/SearchEngineExtension.cs
@@ -0,0 +1,23 @@
+﻿using Oostel.Application.Modules.Hostel.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oostel.Application.RequestFilters
+{
+    public static class SearchEngineExtension
+    {
+        public static IQueryable<HostelDTO> Search(this IQueryable<HostelDTO> hostelDto, string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return hostelDto;
+
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
+
+            return hostelDto.Where(w => w.Street.ToLower().Contains(lowerCaseTerm));
+
+        }
+    }
+}
