847863673e70fc5af56a168a967f5cdfe1396e34
donkennie <ajeigbekehinde160@gmail.com>
2023-09-20 23:22:10 +0100

    add migration

---
+++ oostel.Infrastructure/Migrations/20230920222031_addPriceBudgetEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oostel.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPriceBudgetEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PriceBudgetRange",
+                table: "Hostels",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceBudgetRange",
+                table: "Hostels");
+        }
+    }
+}
