a1d9fc81cde6b11a655a09a6c1fb4c8d21179e06
donkennie <ajeigbekehinde160@gmail.com>
2023-09-30 05:25:41 +0100

    Add migration for the profile picture

--- oostel.Infrastructure/Migrations/20230930035109_MakeChanges.Designer.cs
+++ oostel.Infrastructure/Migrations/20230930035109_MakeChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Oostel.Infrastructure.Data;
@@ -12,9 +13,11 @@ using Oostel.Infrastructure.Data;
 namespace Oostel.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230930035109_MakeChanges")]
+    partial class MakeChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
