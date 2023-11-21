96898a3fd6f4c2764fef86ff16d5b9ddc6e02421
donkennie <ajeigbekehinde160@gmail.com>
2023-11-21 17:27:58 +0100

    to resolve error

--- oostel.Infrastructure/EmailService/EmailParameter.cs
+++ oostel.Infrastructure/EmailService/EmailParameter.cs
@@ -1,12 +1,4 @@
-﻿using MimeKit;
-using System;
-using MailKit.Net.Smtp;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Oostel.Infrastructure.EmailService
+﻿namespace Oostel.Infrastructure.EmailService
 {
     public class EmailParameter
     {
--- oostel.Infrastructure/EmailService/EmailSender.cs
+++ oostel.Infrastructure/EmailService/EmailSender.cs
@@ -1,24 +1,25 @@
-﻿
-using MailKit.Net.Smtp;
-using MailKit.Security;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using MimeKit;
-using Newtonsoft.Json.Linq;
+
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace Oostel.Infrastructure.EmailService
 {
     public class EmailSender : IEmailSender
     {
 
-        //private readonly IConfiguration _config;
-        private readonly EmailConfiguration _mailjetSettings;
+        private readonly IConfiguration _config;
+        // private readonly EmailConfiguration _mailjetSettings;
+        public EmailSender(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
 
-        public EmailSender(IOptions<EmailConfiguration> mailjetSettings)
+        /*public EmailSender(IOptions<EmailConfiguration> mailjetSettings)
         {
             _mailjetSettings = mailjetSettings.Value;
-        }
+        }*/
 
        /* public async Task SendEmailAsync(string email, string subject, string body)
         {
@@ -45,7 +46,7 @@ namespace Oostel.Infrastructure.EmailService
             MailjetResponse response = await client.PostAsync(request);
         }*/
 
-        public async Task SendEmailAsync(EmailParameter emailParameter)
+       /* public async Task SendEmailAsync(EmailParameter emailParameter)
         {
             var message = new MimeMessage();
             message.From.Add(MailboxAddress.Parse(_mailjetSettings.UserName));
@@ -60,6 +61,62 @@ namespace Oostel.Infrastructure.EmailService
             await client.AuthenticateAsync(_mailjetSettings.UserName, _mailjetSettings.Password);
             await client.SendAsync(message);
             await client.DisconnectAsync(true);
+        }*/
+
+
+        public bool SendEmailAsync(string email, string subject, string body)
+        {
+            var client = new RestClientOptions(baseUrl: "https://api.mailgun.net/v3");
+
+            RestRequest request = new RestRequest(resource: "", Method.Post);
+
+            client.Authenticator =
+                new HttpBasicAuthenticator(username:"", password:_config.GetSection(key: "API_KEY").Value);
+
+            request.AddParameter(name: "domain", value: "sandbox2a974a37475a4fb38760dfbc1898ac89.mailgun.org");
+            request.Resource = "{domain}/messages";
+            request.AddParameter(name: "from", value: "Excited User <postmaster@sandbox2a974a37475a4fb38760dfbc1898ac89.mailgun.org>");
+            request.AddParameter(name: "to", value: email);
+            request.AddParameter(name: "subject", value: subject);
+            request.AddParameter(name: "text", value: body);
+            request.Method = Method.Post;
+
+            var response = client.Execute(client);
+
         }
+
+        public async Task SendEmailAsync(string email, string subject, string body)
+        {
+            var client = new RestClient("https://api.mailgun.net/v3");
+
+            var request = new RestRequest("{domain}/messages", Method.Post)
+                .AddUrlSegment("domain", "sandbox2a974a37475a4fb38760dfbc1898ac89.mailgun.org")
+                .AddParameter("from", "Excited User <postmaster@sandbox2a974a37475a4fb38760dfbc1898ac89.mailgun.org>")
+                .AddParameter("to", email)
+                .AddParameter("subject", subject)
+                .AddParameter("text", body);
+
+            // Manually set the Authorization header
+            var apiKey = _config.GetSection("API_KEY").Value;
+            request.AddHeader("Authorization", "Basic " + Convert.ToBase64String(System.Text.Encoding.Default.GetBytes($"api:{apiKey}")));
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine($"Message sent! Response: {response.Content}");
+            }
+            else
+            {
+                Console.WriteLine($"An error occurred: {response.ErrorMessage}");
+            }
+        }
+
+
+
+
+
+
+
     }
 }
