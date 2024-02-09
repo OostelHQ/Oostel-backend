4cbdc491aca31381c4a86c0701866e7204d93493
donkennie <ajeigbekehinde160@gmail.com>
2024-02-09 15:53:17 +0100

    create notification interface

--- oostel.Application/Modules/Notification/Service/INotificationService.cs
+++ oostel.Application/Modules/Notification/Service/INotificationService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Oostel.Application.Modules.Notification.DTOs;
+using Oostel.Common.Constants;
+using Oostel.Common.Helpers;
+using Oostel.Common.Types.RequestFeatures;
+using Oostel.Domain.Notification;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,11 @@ namespace Oostel.Application.Modules.Notification.Service
 {
     public interface INotificationService
     {
+        Task<bool> CreateNotificationAsync(NotificationDTO notificationDTO);
+        Task<ResultResponse<PagedList<Notifications>>> GetNotificationAsync(GetNotificationRequestDTO notificationRequestDTO, PagingParams paginationParameters);
+        Task<bool> MarkNotificationAsReadAsync(string userId, List<string> notificationIds);
+        Task<bool> MarkAllNotificationAsReadAsync(string userId);
+        Task<int> GetNotificationCountAsync(string userId, NotificationType notificationType);
+        Task<bool> DeleteNotificationAsync(string notificationId, NotificationType notificationType);
     }
 }
