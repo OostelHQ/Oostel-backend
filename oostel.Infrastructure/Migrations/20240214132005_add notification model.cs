b9e1dd361ce209f166bade5e2ebb4b394d0135a6
donkennie <ajeigbekehinde160@gmail.com>
2024-02-14 14:24:31 +0100

    add migration

---
+++ oostel.Infrastructure/Migrations/20240214132005_add notification model.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oostel.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnotificationmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "PayInHistories",
+                newName: "AmountPaid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "PayInHistories",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentType",
+                table: "PayInHistories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NotificationType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserProfilePicUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false),
+                    NotificationTypeValueId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PayInHistories_UserId",
+                table: "PayInHistories",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PayInHistories_AspNetUsers_UserId",
+                table: "PayInHistories",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PayInHistories_AspNetUsers_UserId",
+                table: "PayInHistories");
+
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PayInHistories_UserId",
+                table: "PayInHistories");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentType",
+                table: "PayInHistories");
+
+            migrationBuilder.RenameColumn(
+                name: "AmountPaid",
+                table: "PayInHistories",
+                newName: "Amount");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "PayInHistories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
